0690d0568ee60397a659d0bb0e27b21a5e34602a
Csombok Attila <Csombok Attila>
2017-03-25 17:07:07 +0100

    Added tslint to the project

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,29 +1,28 @@
-import {NgModule} from '@angular/core'
-import {BrowserModule} from '@angular/platform-browser'
-import {RouterModule} from '@angular/router'
-import {EventsAppComponent} from './events-app.component.js'
-import {EventsListComponent} from './events/events-list.component.js'
-import {EventThumbnailComponent} from './events/event-thumbnail.component.js'
-import {EventDetailsComponent} from './events/event-details/event-details.component.js'
-import {NavBarComponent} from './nav/nav-bar.component.js'
-import {EventService} from './events/shared/event.service.js'
-import {ToastrService} from './common/toastr.service.js'
-import {appRoutes} from './routes'
-
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {RouterModule} from "@angular/router";
+import {ToastrService} from "./common/toastr.service.js";
+import {EventsAppComponent} from "./events-app.component.js";
+import {EventDetailsComponent} from "./events/event-details/event-details.component.js";
+import {EventThumbnailComponent} from "./events/event-thumbnail.component.js";
+import {EventsListComponent} from "./events/events-list.component.js";
+import {EventService} from "./events/shared/event.service.js";
+import {NavBarComponent} from "./nav/nav-bar.component.js";
+import {appRoutes} from "./routes";
 
 @NgModule({
-    imports: [ 
+    imports: [
         BrowserModule,
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes),
      ],
     declarations: [
-        EventsAppComponent, 
+        EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
         EventDetailsComponent,
         NavBarComponent],
     providers: [EventService, ToastrService],
-    bootstrap: [EventsAppComponent]
+    bootstrap: [EventsAppComponent],
 })
 
 export class AppModule{
--- app/nav/nav-bar.component.ts
+++ app/nav/nav-bar.component.ts
@@ -1,13 +1,13 @@
-import {Component} from '@angular/core'
+import {Component} from "@angular/core"
 
 @Component({
-    selector: 'nav-bar',
-    templateUrl: 'app/nav/nav-bar.component.html',
+    selector: "nav-bar",
+    templateUrl: "app/nav/nav-bar.component.html",
     styles: [ `
         .nav.navbar-nav {font-size: 15px;}
         #searchForm {margin-right: 100px;}
     `
-    ]
+    ],
 })
 
 export class NavBarComponent {
